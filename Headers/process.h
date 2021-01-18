b39f5e41384abad96724145fc70174ef462334a9
Tom <tom-vautray@hotmail.fr>
2021-01-18 18:27:25 +0100

    Works well
    Tested under windows only, has to under linux

---
+++ Headers/process.h
@@ -0,0 +1,12 @@
+#include <string>
+
+using namespace std;
+
+//Is windows the compiling os ?
+#if __has_include(<unistd.h>)
+#else
+FILE* __cdecl popen(const char* _Command, const char* _Mode);
+int __cdecl pclose(FILE* _Stream);
+#endif
+
+string _system(const char* cmd);
