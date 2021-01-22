b3ed9aa36c8dd9fbc5130e42cdacc7f74df4d4df
Tom <tom-vautray@hotmail.fr>
2021-01-22 13:50:25 +0100

    Correct way to detect linux in cpp as well

--- Headers/process.h
+++ Headers/process.h
@@ -3,8 +3,7 @@
 using namespace std;
 
 //Is windows the compiling os ?
-#if __has_include(<unistd.h>)
-#else
+#ifndef __linux__
 FILE* __cdecl popen(const char* _Command, const char* _Mode);
 int __cdecl pclose(FILE* _Stream);
 #endif
